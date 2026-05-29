12674f39feff0c43ca66b59680c9b82182f2ac41
nicklllson <nicklllson.off@gmail.com>
2026-05-29 12:39:11 +0500

    fix: fixed email service

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as nodemailer from 'nodemailer';
+import { Resend } from 'resend';
 
 @Injectable()
 export class MailService {
